298d77162c11b35e49ad43a6d56da210ccbffa09
John Garner <jdextergarneriv@gmail.com>
2020-11-13 13:37:16 -0500

    created html get routes

--- server.js
+++ server.js
@@ -2,5 +2,11 @@ const express = require("express");
 const app = express();
 var bodyParser = require("body-parser");
 var PORT = process.env.PORT || 8080;
+var path = require("path");
 
-require("");
+// require("./routing/api-routes.js")(app);
+// require("./routing/html-routes.js")(app);
+
+app.listen(PORT, function () {
+	console.log("APP LISTENING ON PORT: " + PORT);
+});
